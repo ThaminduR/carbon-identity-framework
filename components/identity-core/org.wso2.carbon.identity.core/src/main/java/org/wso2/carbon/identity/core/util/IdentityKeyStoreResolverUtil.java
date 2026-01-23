da40fe5ee5b57a9f7df19c88ae12af0a4f7b7954
ThaminduR <thamindu.randil@gmail.com>
2026-01-23 10:41:26 +0530

    Fix keystore file name generation to include context in extension retrieval

--- components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/util/IdentityKeyStoreResolverUtil.java
+++ components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/util/IdentityKeyStoreResolverUtil.java
@@ -73,7 +73,7 @@ public class IdentityKeyStoreResolverUtil {
         }
 
         // Add the keystore extension
-        return ksName + KeystoreUtils.getKeyStoreFileExtension(context, tenantDomain);
+        return ksName + KeystoreUtils.getKeyStoreFileExtension(ksName, tenantDomain);
     }
 
     /**
